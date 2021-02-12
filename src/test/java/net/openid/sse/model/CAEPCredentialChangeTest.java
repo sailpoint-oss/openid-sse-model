95f3e9f347a113ca159b231537d9320fbbb5ce98
Matt Domsch <matt.domsch@sailpoint.com>
2021-02-11 19:57:20 -0600

    Cleanup tests for CAEPAssuranceLevelChange

--- src/test/java/net/openid/sse/model/CAEPCredentialChangeTest.java
+++ src/test/java/net/openid/sse/model/CAEPCredentialChangeTest.java
@@ -12,46 +12,11 @@ import static org.junit.Assert.assertEquals;
 
 public class CAEPCredentialChangeTest {
     /**
-     * Figure 4: Example: OIDC ID Token Claims Change
+     * Figure 6: Example: Provisioning a new FIDO2 authenticator
      */
 
     @Test
-    public void Figure4() throws ParseException {
-        SubjectIdentifier subj = new SubjectIdentifier.Builder()
-                .subjectType(SubjectIdentifierTypes.JWT_ID)
-                .issuer("https://idp.example.com/987654321/")
-                .jwtID("f61t6e20zdo3px56gepu8rzlsp4c1dpc0fx7")
-                .build();
-
-        JSONObject claims = new JSONObject();
-        claims.put("role", "ro-admin");
-
-        SSEvent evt = new CAEPTokenClaimsChange.Builder()
-                .eventTimestamp(1600975810L)
-                .subject(subj)
-                .claim("claims", claims)
-                .build();
-
-        JWTClaimsSet set = new JWTClaimsSet.Builder()
-                .issuer("https://idp.example.com/987654321/")
-                .jwtID("9afce1e4e642b165fcaacdd0e7aa4903")
-                .issueTime(DateUtils.fromSecondsSinceEpoch(1600976590L))
-                .audience("https://sp.example2.net/caep")
-                .claim(SEToken.EVENTS_CLAIM, evt)
-                .build();
-
-        final String figure_text = "";
-
-        final JSONObject figureJson = new JSONObject(JSONObjectUtils.parse(figure_text));
-        final JSONObject setJson = new JSONObject(set.toJSONObject());
-        assertEquals(figureJson, setJson);
-    }
-    /**
-     * Figure 5: Example: SAML Assertion Claims Change
-     */
-
-    @Test
-    public void Figure5() throws ParseException {
+    public void Figure6() throws ParseException {
         SubjectIdentifier subj = new SubjectIdentifier.Builder()
                 .subjectType(SubjectIdentifierTypes.ISSUER_SUBJECT)
                 .issuer("https://idp.example.com/3456789/")
@@ -87,6 +52,9 @@ public class CAEPCredentialChangeTest {
                 "                   \"iss\": \"https://idp.example.com/3456789/\",\n" +
                 "                   \"sub\": \"jane.smith@example.com\"\n" +
                 "               },\n" +
+//                "               \"current_level\": \"nist-aal2\",\n" +
+//                "               \"previous_level\": \"nist-aal1\",\n" +
+//               "               \"change_direction\": \"increase\",\n" +
                 "               \"credential_type\": \"fido2-roaming\",\n" +
                 "               \"change_type\": \"create\",\n" +
                 "               \"fido2_aaguid\": \"accced6a-63f5-490a-9eea-e59bc1896cfc\",\n" +
@@ -95,7 +63,7 @@ public class CAEPCredentialChangeTest {
                 "               \"event_timestamp\": 1600975811\n" +
                 "           }\n" +
                 "       }\n" +
-                "   }\n";
+                "   }";
 
         final JSONObject figureJson = new JSONObject(JSONObjectUtils.parse(figure_text));
         final JSONObject setJson = new JSONObject(set.toJSONObject());

5848c04e462be014c77bdcc061614a97fd0931ba
Matt Domsch <matt.domsch@sailpoint.com>
2021-02-14 16:47:07 -0600

    Add validation of exp and type members to SEToken

--- src/main/java/net/openid/sse/model/SEToken.java
+++ src/main/java/net/openid/sse/model/SEToken.java
@@ -2,24 +2,54 @@ package net.openid.sse.model;
 
 import com.nimbusds.jwt.JWTClaimsSet;
 
-import java.util.Map;
+import java.text.ParseException;
 
 public class SEToken
 {
     public static final String EVENTS_CLAIM = "events";
+    private static final String TYP_CLAIM = "typ";
+    private static final String EXP_CLAIM = "exp";
+    private static final String TYP_VALUE = "secevent+jwt";
 
     private SEToken() {}
 
-    public static void validate(JWTClaimsSet set) throws ValidationException {
-        Object o = set.getClaim(EVENTS_CLAIM);
+    /* openid-sse-profile-1_0.txt section 11.1.4 */
+    private static void validateTyp(final JWTClaimsSet set) throws ParseException, ValidationException {
+        String typ;
+        typ = set.getStringClaim(TYP_CLAIM);
+
+        if (null == typ) {
+            /* OK to be missing */
+            return;
+        }
+
+        if (!TYP_VALUE.equals(typ)) {
+            throw new ValidationException("SET events claim typ value must be " + TYP_VALUE + ".");
+        }
+    }
+
+    /* openid-sse-profile-1_0.txt section 11.1.5 */
+    private static void validateExp(final JWTClaimsSet set) throws ValidationException {
+        Object exp;
+        exp = set.getClaim(EXP_CLAIM);
+        if (null != exp) {
+            throw new ValidationException("SET events must not have an exp claim.");
+        }
+    }
+
+    /* openid-sse-profile-1_0.txt section 11.1.7 */
+    private static void validateEventsClaim(final JWTClaimsSet set) throws ParseException, ValidationException {
+        Object o;
+        o = set.getJSONObjectClaim(EVENTS_CLAIM);
+
         if (null == o) {
             throw new ValidationException("SETs must contain an events member.");
         }
-        if (!(o instanceof Map)) {
-            throw new ValidationException("SET events member must be a Map.");
-        }
-        // FIXME this is where I leave off
-        // SSEvent evt = new SSEvent((HashMap<String, Object>) o);
-        // evt.validate() here when it's ready
+    }
+
+    public static void validate(final JWTClaimsSet set) throws ParseException, ValidationException {
+        validateTyp(set);
+        validateExp(set);
+        validateEventsClaim(set);
     }
 }

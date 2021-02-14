bcaca79b41f5c33faaa6b2f783a5497253fb31c6
Matt Domsch <matt.domsch@sailpoint.com>
2021-02-14 00:12:35 -0600

    Genericize SSEvent
    We have a problem with the Builder pattern used:
    https://javadevcentral.com/effective-java-builder-pattern
    
    that arises because we're using inheritance, but Foo.Builder().chain().of().additions().build() must go from deepest subclass methods to highest superclass methods, and must return the top level superclass object type, which will then make it hard to do any other reasoning about it.
    
    As suggested here:
    http://egalluzzo.blogspot.com/2010/06/using-inheritance-with-fluent.html
    
    we employ Generics and abstract classes at the superclass level in SSEvent, which lets us return the subclass object in build().
    
    We added an eventType member, so we also need to add setters and getters, equals() and hashCode();

--- src/main/java/net/openid/sse/model/SSEvent.java
+++ src/main/java/net/openid/sse/model/SSEvent.java
@@ -3,6 +3,7 @@ package net.openid.sse.model;
 import com.nimbusds.jose.shaded.json.JSONObject;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * A Shared Signals Event (SSE Event).
@@ -11,70 +12,119 @@ import java.util.Map;
  * @author matt.domsch
  *
  */
-public class SSEvent extends JSONObject {
+public abstract class SSEvent extends JSONObject {
 
-	public static class Builder {
+	private static final String SUBJECT_MEMBER     = "subject";
+	private static final String STATUS_MEMBER      = "status";
+	private static final String REASON_MEMBER      = "reason";
+	private static final String PROPERTIES_MEMBER  = "properties";
+	private static final String IPADDRESS_MEMBER   = "ip_address";
+	private static final String ID_MEMBER   	   = "id";
 
-		private static final String SUBJECT_MEMBER     = "subject";
-		private static final String STATUS_MEMBER      = "status";
-		private static final String REASON_MEMBER      = "reason";
-		private static final String PROPERTIES_MEMBER  = "properties";
-		private static final String IPADDRESS_MEMBER   = "ip_address";
-		private static final String ID_MEMBER   	   = "id";
+	private SSEventTypes eventType;
 
-		private String eventTypeName = "";
-		protected final JSONObject members = new JSONObject();
+	public SSEventTypes getEventType() {
+		return eventType;
+	}
 
-		public Builder eventType(final SSEventTypes eventType) {
-			this.eventTypeName = eventType.toString();
-			return this;
+	public void setEventType(SSEventTypes eventType) {
+		this.eventType = eventType;
+	}
+
+	protected SSEvent() {}
+
+	protected abstract static class Builder<T extends SSEvent, B extends Builder<T, B>> {
+
+		protected T members;
+		protected B thisObj;
+
+		protected Builder() {
+			members = createObj();
+			thisObj = getThis();
 		}
 
-		public Builder eventType(final String eventType) {
-			this.eventTypeName = eventType;
-			return this;
+		protected Builder(final SSEventTypes eventType) {
+			members = createObj();
+			thisObj = getThis();
+			this.eventType(eventType);
 		}
 
-		public Builder subject(final Map<String, Object> sub) {
+		public T build() {
+			T t = createObj();
+			t.setEventType(eventType);
+			t.put(eventType.toString(), members);
+			return t;
+		}
+
+		protected abstract T createObj();
+		protected abstract B getThis();
+
+
+		private SSEventTypes eventType;
+
+		public B eventType(final SSEventTypes eventType) {
+			this.eventType = eventType;
+			return thisObj;
+		}
+
+		public B subject(final Map<String, Object> sub) {
 			members.put(SUBJECT_MEMBER, sub);
-			return this;
+			return thisObj;
 		}
 
-		public Builder status(final String status) {
+		public B status(final String status) {
 			members.put(STATUS_MEMBER, status);
-			return this;
+			return thisObj;
 		}
 
-		public Builder reason(final String reason) {
-
+		public B reason(final String reason) {
 			members.put(REASON_MEMBER, reason);
-			return this;
+			return thisObj;
 		}
-		public Builder properties(final Map<String, Object> properties) {
+
+		public B properties(final Map<String, Object> properties) {
 			members.put(PROPERTIES_MEMBER, properties);
-			return this;
+			return thisObj;
 		}
 
-		public Builder ipAddress(final String ipAddress) {
+		public B ipAddress(final String ipAddress) {
 			members.put(IPADDRESS_MEMBER, ipAddress);
-			return this;
+			return thisObj;
 		}
 
-		public Builder id(final String ipAddress) {
+		public B id(final String ipAddress) {
 			members.put(ID_MEMBER, ipAddress);
-			return this;
+			return thisObj;
 		}
 
-		public Builder claim(final String claim, final Object o) {
+		public B claim(final String claim, final Object o) {
 			members.put(claim, o);
-			return this;
-		}
-
-		public SSEvent build() {
-			SSEvent event = new SSEvent();
-			event.put(eventTypeName, members);
-			return  event;
+			return thisObj;
 		}
 	}
+
+	public void validate() throws ValidationException {
+		for (String k : this.keySet()) {
+			if (SSEventTypes.contains(k))
+				return;
+		}
+		throw new ValidationException("SSEvent eventTypeName not in SSEventTypes.");
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!super.equals(obj)) {
+			return false;
+		}
+		SSEvent event = (SSEvent) obj;
+		return eventType.equals(event.getEventType());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this, eventType);
+	}
 }
+
+
 

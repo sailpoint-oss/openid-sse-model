228efb1b35950618388526863ed5fe1bed6e8dc4
Matt Domsch <matt.domsch@sailpoint.com>
2021-02-25 19:09:47 -0600

    Add SPDX-License-Identifier and copyright to all relevant files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 SailPoint Technologies, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 module net.openid.sse.model {
     exports net.openid.sse.model;
     requires transitive com.nimbusds.jose.jwt;
--- src/test/java/net/openid/sse/model/CAEPAssuranceLevelChangeTest.java
+++ src/test/java/net/openid/sse/model/CAEPAssuranceLevelChangeTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 SailPoint Technologies, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package net.openid.sse.model;
 
 import com.nimbusds.jose.shaded.json.JSONObject;

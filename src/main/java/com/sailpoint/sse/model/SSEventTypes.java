e23cf59ca4f76b60a61479481f770f4073cac382
Matt Domsch <matt.domsch@sailpoint.com>
2021-03-31 13:50:26 -0500

    Add class names implementing enum SSEventTypes to enum private data
    We'll need this when we go to instantiate new class hierarchies while
    parsing an incoming JSON stream.  We can use this to look up the class
    we'll need to instantiate by the event type URL name.
    
    Signed-off-by: Matt Domsch <matt.domsch@sailpoint.com>

--- src/main/java/com/sailpoint/sse/model/SSEventTypes.java
+++ src/main/java/com/sailpoint/sse/model/SSEventTypes.java
@@ -6,35 +6,41 @@
 
 package com.sailpoint.sse.model;
 
+import com.sailpoint.sse.model.caep.*;
+import com.sailpoint.sse.model.risc.*;
+import com.sailpoint.sse.model.sse.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
+
 public enum SSEventTypes {
 
-    RISC_ACCOUNT_CREDENTIAL_CHANGE_REQUIRED("account-credential-change-required", SSESpecs.RISC_PREFIX),
-    RISC_ACCOUNT_PURGED("account-purged", SSESpecs.RISC_PREFIX),
-    RISC_ACCOUNT_DISABLED("account-disabled", SSESpecs.RISC_PREFIX),
-    RISC_ACCOUNT_ENABLED("account-enabled", SSESpecs.RISC_PREFIX),
-    RISC_IDENTIFIER_CHANGED("identifier-changed", SSESpecs.RISC_PREFIX),
-    RISC_IDENTIFIER_RECYCLED("identifier-recycled", SSESpecs.RISC_PREFIX),
-    RISC_OPT_IN("opt-in", SSESpecs.RISC_PREFIX),
-    RISC_OPT_OUT_INITIATED("opt-out-initiated", SSESpecs.RISC_PREFIX),
-    RISC_OPT_OUT_CANCELLED("opt-out-cancelled", SSESpecs.RISC_PREFIX),
-    RISC_OPT_OUT_EFFECTIVE("opt-out-effective", SSESpecs.RISC_PREFIX),
-    RISC_RECOVERY_ACTIVATED("recovery-activated", SSESpecs.RISC_PREFIX),
-    RISC_RECOVERY_INFORMATION_CHANGED("recovery-information-changed", SSESpecs.RISC_PREFIX),
-    RISC_SESSIONS_REVOKED("sessions-revoked", SSESpecs.RISC_PREFIX),
+    RISC_ACCOUNT_CREDENTIAL_CHANGE_REQUIRED("account-credential-change-required", SSESpecs.RISC_PREFIX, RISCAccountCredentialChangeRequired.class),
+    RISC_ACCOUNT_PURGED("account-purged", SSESpecs.RISC_PREFIX, RISCAccountPurged.class),
+    RISC_ACCOUNT_DISABLED("account-disabled", SSESpecs.RISC_PREFIX, RISCAccountDisabled.class),
+    RISC_ACCOUNT_ENABLED("account-enabled", SSESpecs.RISC_PREFIX, RISCAccountEnabled.class),
+    RISC_IDENTIFIER_CHANGED("identifier-changed", SSESpecs.RISC_PREFIX, RISCIdentifierChanged.class),
+    RISC_IDENTIFIER_RECYCLED("identifier-recycled", SSESpecs.RISC_PREFIX, RISCIdentifierRecycled.class),
+    RISC_OPT_IN("opt-in", SSESpecs.RISC_PREFIX, RISCOptIn.class),
+    RISC_OPT_OUT_INITIATED("opt-out-initiated", SSESpecs.RISC_PREFIX, RISCOptOutInitiated.class),
+    RISC_OPT_OUT_CANCELLED("opt-out-cancelled", SSESpecs.RISC_PREFIX, RISCOptOutCancelled.class),
+    RISC_OPT_OUT_EFFECTIVE("opt-out-effective", SSESpecs.RISC_PREFIX, RISCOptOutEffective.class),
+    RISC_RECOVERY_ACTIVATED("recovery-activated", SSESpecs.RISC_PREFIX, RISCRecoveryActivated.class),
+    RISC_RECOVERY_INFORMATION_CHANGED("recovery-information-changed", SSESpecs.RISC_PREFIX, RISCRecoveryInformationChanged.class),
+    RISC_SESSIONS_REVOKED("sessions-revoked", SSESpecs.RISC_PREFIX, RISCSessionsRevoked.class),
 
-    CAEP_IPADDR_CHANGED("ip-address-changed", SSESpecs.CAEP_PREFIX),
-    CAEP_TOKEN_REVOCATION("token-revocation", SSESpecs.CAEP_PREFIX),
-    CAEP_SESSION_REVOKED("session-revoked", SSESpecs.CAEP_PREFIX),
-    CAEP_TOKEN_CLAIMS_CHANGE("token-claims-change", SSESpecs.CAEP_PREFIX),
-    CAEP_CREDENTIAL_CHANGE("credential-change", SSESpecs.CAEP_PREFIX),
-    CAEP_ASSURANCE_LEVEL_CHANGE("assurance-level-change", SSESpecs.CAEP_PREFIX),
-    CAEP_DEVICE_COMPLIANCE_CHANGE("device-compliance-change", SSESpecs.CAEP_PREFIX),
+    CAEP_IPADDR_CHANGED("ip-address-changed", SSESpecs.CAEP_PREFIX, CAEPIPAddrChanged.class),
+    //Where is this defined?
+    //CAEP_TOKEN_REVOCATION("token-revocation", SSESpecs.CAEP_PREFIX.class),
+    CAEP_SESSION_REVOKED("session-revoked", SSESpecs.CAEP_PREFIX, CAEPSessionRevoked.class),
+    CAEP_TOKEN_CLAIMS_CHANGE("token-claims-change", SSESpecs.CAEP_PREFIX, CAEPTokenClaimsChange.class),
+    CAEP_CREDENTIAL_CHANGE("credential-change", SSESpecs.CAEP_PREFIX, CAEPCredentialChange.class),
+    CAEP_ASSURANCE_LEVEL_CHANGE("assurance-level-change", SSESpecs.CAEP_PREFIX, CAEPAssuranceLevelChange.class),
+    CAEP_DEVICE_COMPLIANCE_CHANGE("device-compliance-change", SSESpecs.CAEP_PREFIX, CAEPDeviceComplianceChange.class),
 
-    SSE_VERIFICATION("verification", SSESpecs.SSE_PREFIX),
-    SSE_STREAM_UPDATED("stream-updated", SSESpecs.SSE_PREFIX);
+    SSE_VERIFICATION("verification", SSESpecs.SSE_PREFIX, SSEVerification.class),
+    SSE_STREAM_UPDATED("stream-updated", SSESpecs.SSE_PREFIX, null);
 
 
     // Name space prefix string for Shared Signals and Event types.
@@ -48,9 +54,11 @@ public enum SSEventTypes {
     }
 
     private final String name;
+    private final Class<? extends SSEvent> cls;
 
-    SSEventTypes(final String s, final String spec) {
+    SSEventTypes(final String s, final String spec, final Class<? extends SSEvent> cls) {
         name = SSE_URL_PREFIX + spec + "/event-type/" + s;
+        this.cls = cls;
     }
 
     public static SSEventTypes valueOfLabel(String name) {
@@ -69,6 +77,8 @@ public enum SSEventTypes {
     public String toString() {
         return this.name;
     }
+
+    public Class<? extends SSEvent> getCls() { return this.cls; }
 
 }
 

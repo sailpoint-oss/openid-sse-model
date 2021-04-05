c10f0293cebe8e888671a921b9b45c773693912e
Matt Domsch <matt.domsch@sailpoint.com>
2021-04-05 09:09:38 -0500

    CAEP event_timestamp is optional
    after discussion in the WG again, event_timestamp is once again
    optional. Ignore it's absence.
    
    Signed-off-by: Matt Domsch <matt.domsch@sailpoint.com>

--- src/main/java/com/sailpoint/sse/model/caep/CAEPBaseEvent.java
+++ src/main/java/com/sailpoint/sse/model/caep/CAEPBaseEvent.java
@@ -38,7 +38,8 @@ public abstract class CAEPBaseEvent extends SSEvent {
 
         Object eventTimestamp = members.get(EVENT_TIMESTAMP_MEMBER);
         if (null == eventTimestamp) {
-            throw new ValidationException("CAEP Events must have an event_timestamp member");
+            // event_timestamp is an optional member. Ignore it's absence.
+            return;
         }
 
         if (!(eventTimestamp instanceof Long)) {
